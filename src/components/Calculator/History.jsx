a8f5903013fc6ed5538913bedf05c7d29399b224
Budimir Sukhovey <budimir2003@gmail.com>
2023-12-11 20:25:59 +0300

    adding new styles

--- src/components/Calculator/History.jsx
+++ src/components/Calculator/History.jsx
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './History.css'
 import {Context} from "../../index";
 import {observer} from "mobx-react-lite";
@@ -13,17 +13,21 @@ const History = observer((props) => {
     }
 
     return (
-        <div>
-            <h2 className="HistoryWrapper">История операций</h2>
-            <ul className="HistoryList">
-                {history.history.map((expression, index) => (
-                    <li key={index} className="HistoryItem">{expression}</li>
-                ))}
-            </ul>
-            <button onClick={clearHistory}>Очистить историю</button>
-            <Link to="/" className="BackLink">Вернуться к калькулятору</Link>
+        <div className="containerHistory">
+            <div className="historyTable">
+
+                <h2 className="HistoryWrapper">История операций</h2>
+                <ul className="HistoryList">
+                    {history.history.map((expression, index) => (
+                        <li key={index} className="HistoryItem">{expression}</li>
+                    ))}
+                </ul>
+                <button onClick={clearHistory}>Очистить историю</button>
+                <Link to="/" className="BackLink">Вернуться к калькулятору</Link>
+            </div>
         </div>
-    );
+    )
+        ;
 });
 
 export default History;
